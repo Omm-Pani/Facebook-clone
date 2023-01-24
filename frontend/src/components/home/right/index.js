a37d4d0b7b11d91e6691f1a0a8298aea692f443b
Omm-Pani <ommpani9@gmail.com>
2023-01-24 22:12:48 +0530

    stories part created

--- frontend/src/components/home/right/index.js
+++ frontend/src/components/home/right/index.js
@@ -2,7 +2,13 @@ import "./styles.css";
 export default function RightHome() {
   return (
     <div className="right_home">
-      <div className="heading"></div>
+      <div className="heading">Group conversations</div>
+      <div className="right_item hover1">
+        <div className="small_circle">
+          <i className="plus_icon"></i>
+        </div>
+        <span>Create New Group</span>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -2,13 +2,18 @@ import { useSelector } from "react-redux";
 import Header from "../../components/header";
 import LeftHome from "../../components/home/left";
 import RightHome from "../../components/home/right";
+import Stories from "../../components/home/stories";
+import "./styles.css";
 
 export default function Home() {
   const { user } = useSelector((user) => ({ ...user }));
   return (
-    <div>
+    <div className="home">
       <Header />
       <LeftHome user={user} />
+      <div className="home_middle">
+        <Stories />
+      </div>
       <RightHome />
     </div>
   );
