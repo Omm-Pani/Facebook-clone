68e703ba68b301311cb909fdba846563ca92e2f1
Omm-Pani <ommpani9@gmail.com>
2023-03-20 11:04:16 +0530

    creste and upload posts created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,15 +8,32 @@ import Activate from "./pages/home/activate";
 import Reset from "./pages/reset";
 import CreatePostPopup from "./components/createPostPopup";
 import { useSelector } from "react-redux";
+import { useReducer, useState } from "react";
+function reducer(state, action) {
+  switch (action.type) {
+    case "POST_REQUEST":
+      return { ...state, loading: true, error: "" };
+    case "POST_SUCCESS":
+      return { ...state, loading: false, posts: action.payload, error: "" };
+    case "POST_ERROR":
+      return { ...state, loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+}
+
 function App() {
+  const [visible, setVisible] = useState(false);
   const { user } = useSelector((state) => ({ ...state }));
+  const { posts, loading, error } = useReducer(reducer, loading);
   return (
     <div>
-      <CreatePostPopup user={user} />
+      {visible && <CreatePostPopup user={user} setVisible={setVisible} />}
       <Routes>
         <Route element={<LoggedinRoutes />}>
           <Route path="/profile" element={<Profile />} exact />
-          <Route path="/" element={<Home />} exact />
+          <Route path="/" element={<Home setVisible={setVisible} />} exact />
           <Route path="/activate/:token" element={<Activate />} exact />
         </Route>
         <Route element={<NotLoggedinRoutes />}>
